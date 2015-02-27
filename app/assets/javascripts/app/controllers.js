be98be4f3c6d94503ed78b47642743ec49d975ad
Ryan Luedecke <ryan@sumojerky.com>
2015-02-27 16:02:40 -0600

    Image upload and post is working I think

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -1,3 +1,14 @@
 angular.module('app.controller',[])
-    .controller('AppController', function($scope) {
+    .controller('AppController', function($scope, $http) {
+
+    console.log('controller initialized');
+
+    // Called when Accept button is clicked
+	// send photo to the server
+	$scope.storePhoto = function(photo) {
+		console.log("Accept button clicked")
+		$http.post('http://tiny-pizza-server.herokuapp.com/collections/progress-pics', {data: photo} );
+
+	};
+
     });
