75969c10428e5811a5926c27b58766fd33dee53c
Ryan Luedecke <ryan@sumojerky.com>
2015-02-27 19:59:20 -0600

    Tried to add ui router. Failed

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -11,4 +11,18 @@ angular.module('app.controller',[])
 
 		};
 
+	    })
+
+    .controller('PhotoCtrl', function($scope, $http) {
+	    console.log('controller initialized');
+
+	    // Called when Accept button is clicked
+		// send photo to the server
+		$scope.storePhoto = function(photo) {
+			console.log(photo)
+			console.log("Accept button clicked")
+			$http.post('/users/1/pictures.json', { picture: { photo: photo } } );
+
+		};
+
 	    });
