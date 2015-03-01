c0dfc2c41bdeb3c09093f8f2e7a4dcaf712c097e
jjjjustin <justinsitarz@gmail.com>
2015-03-01 13:22:42 -0600

    worked on rspec tests

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe PicturesController, type: :controller do
 
+  describe 'exists?' do
+    it "should exist" do
+      expect(PicturesController).to_not be_nil
+    end
+  end
+
+  describe 'run' do
+    let(:picture) { PicturesController.new }
+
+    it 'responds to create' do
+      expect(picture).to respond_to(:create)
+    end
+  end
 end
+
+
+
+
+
