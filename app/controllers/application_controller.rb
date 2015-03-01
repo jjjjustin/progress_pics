26a05a9efe555c8ee6a86cdaa7bd087a36f16030
jjjjustin <justinsitarz@gmail.com>
2015-03-01 15:30:34 -0600

    added log out feature to landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :authenticate_user!
+
+  def after_sign_out_path_for(current_user)
+    landing_path
+  end
+
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :user_name
+    end
 end
+
+
+
