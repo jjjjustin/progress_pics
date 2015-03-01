87a2821603c9974742e80e924af4318df6516963
jjjjustin <justinsitarz@gmail.com>
2015-03-01 15:04:46 -0600

    added tests

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe PicturesController, type: :controller do
+  let(:photo) { Picture.new(caption: 'test', photo: '1', user_id: '1', created_at: '1') }
+  let(:imagee) { Picture.new(caption: 'spec', photo: 'spec') }
+  let(:picture) { photo }
 
   describe 'exists?' do
     it "should exist" do
@@ -8,16 +11,18 @@ RSpec.describe PicturesController, type: :controller do
     end
   end
 
-  describe 'run' do
-    let(:picture) { PicturesController.new }
+  describe 'index' do
 
-    it 'responds to create' do
-      expect(picture).to respond_to(:create)
+
+    it 'responds to index' do
+      expect(Picture.all).to respond_to(:index)
+    end
+
+    it 'renders json' do
+      expect(photo.index).to render_template(Hash)
     end
   end
 end
-
-
 
 
 
