5e9f89a099eb96882b64d944d1f80f5cb2ab329f
jjjjustin <justinsitarz@gmail.com>
2015-03-01 15:38:29 -0600

    added logout to javascript

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,16 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_filter :authenticate_user!
 
-  def after_sign_out_path_for(current_user)
+  def after_sign_out_path_for(resource)
     landing_path
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    root_path
   end
 
 
